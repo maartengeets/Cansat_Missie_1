bd8fe9d6b45a424a9cfce14649acefabbb4d1fd8
maartengeets <maartengeets08@gmail.com>
2025-10-24 22:11:01 +0200

    Ik heb de code aangepast. De code leest de BMP280 sensor uit en publiceert de data in een csv string op een mqtt broker.

--- src/main.cpp
+++ src/main.cpp
@@ -1,200 +1,188 @@
+// including libraries
 #include <Arduino.h>
-#include <FS.h>
-#include <SD.h>
-#include <SPI.h>
-#include <Secrets.h>
-#include <Wire.h>
+#include <ArduinoMqttClient.h>
+#include <WiFi.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
-#include <WiFi.h>
-#include <ArduinoMqttClient.h>
-#include "sd_utils.h"
+#include <secrets.h> // secrets file invoegen
 
-#define FILE_NAME "/Maarten_Test.txt"
-#define SD_CSPIN  25
-
-// WiFi Client definition
+// wificlient definieren
 WiFiClient wifiClient;
 
-// MQTT Client definition
+// mqttclient definieren
 MqttClient mqttClient(wifiClient);
 
-// Set the value of the sea level pressure correct at your location
-// You can find it at https://www.meteo.be/nl/weer/waarnemingen/belgie 
-// if you do so, the height will be calculated approx. correctly
-// + or - 8m hight difference is normal, as the sensor has a deviation
+// stel de waarde van de luchtdruk op zeeniveau in voor jouw locatie
+// je kunt deze vinden op https://www.meteo.be/nl/weer/waarnemingen/belgie
+// als je dat doet, wordt de hoogte ongeveer correct berekend
+// een afwijking van ±8 m is normaal, omdat de sensor een afwijking heeft
 #define SEALEVELPRESSURE_HPA (1007.1)
 
-#define BMP280_ADR  0x76 // Set I2C address of BMP280 sensor
-                         // Some sensors will use address 0x77      
+#define BMP280_ADR  0x76 // zet I2C address van de BMP280 sensor
+                         // sommige sensoren gebruiken het adress 0x77 
 
-// In this example, we'll use differnt than default pins for I2C connection
-#define SDA_2   32 // Use this PIN as secondary I2C SDA connection
-#define SCL_2   33 // Use this PIN as secondary I2C SCL connection
+// in deze oefening, gebruiken we andere dan de default I2C pinnen
+#define SDA_2   32 // gebruik deze PIN als tweede I2C SDA connectie
+#define SCL_2   33 // gebruik deze PIN als tweede I2C SCL connectie
 #define DELAY_TIME  1000
 
-TwoWire I2Ctwo = TwoWire(1);  // Create I2Ctwo object on ESP32
-                              // This is needed as we will be using a second
-                              // I2C connection
-Adafruit_BMP280 bmp( &I2Ctwo ); // declare an object bmp where data from the 
-                                // bmp280 sensor can be written into the
-                                // I2C object I2Ctwo previously declared.
+TwoWire I2Ctwo = TwoWire(1);  // maak I2Ctwo object op ESP32
+                              // dees is nodig als we een tweede
+                              // I2C connection willen gebruiken
+Adafruit_BMP280 bmp( &I2Ctwo ); // declareer een object bmp waar data van de 
+                                // bmp280 sensor kan geschreven worden in de
+                                // I2C object I2Ctwo eerder gedeclareerd.
 
-bool bmp_connected = false; // A global var to indicate throughout the program
-                            // if the bmp sensor is connected or not
+bool bmp_connected = false; // een globale var om doorheen het programma te checken
+                            // of de bmp sensor geconnecteerd is
 
-/* Declare functions
-void appendFile(fs::FS &fs, const char * path, const char * message);
-void deleteFile(fs::FS &fs, const char * path);
-int  ConnectSD();
-*/
-bool I2C_check(TwoWire *bus, byte adress);
+// vooruitdeclaties (prototypes) zodat loop() deze functies kan aanroepen
 void printValues();
-
-// Define constants for error messages
-const char* ERROR_BMP_NOT_FOUND = "Could not find a valid BMP280 sensor, check wiring, address, sensor ID!";
-const char* ERROR_MQTT_CONNECTION_FAILED = "MQTT connection failed! Error code = ";
+String csv_string_data();
+void mqtt_pub_csv(const char *topic, String str_csv);
+bool I2C_check(TwoWire *bus, byte address);
 
 void setup() {
-  
-  char msg[120];
-  
-    // start serial monitor
-    Serial.begin( 115200 );
-    Serial.println( "Start of Program" );
-    Serial.println( "----------------" );
-    Serial.println("Setting BMP280 up:");
-    while(!Serial) {
-        delay( 10 );
-    }    // time to get serial running
-    Serial.println( "BMP280 test" );
-    
-    // Connect BMP280 sensor with non-default I2C connection
-    I2Ctwo.begin(SDA_2, SCL_2);  // SDA=32, SCL=33
+  // initialiseren van de seriele poort en wachten tot het opent
+  Serial.begin(115200);
+  while (!Serial) {
+    ; // wacht voor de seriele poort voor te connecteren
+  }
 
-    // Connect the bmp280 sensor
+  Serial.println("Begin programma");
+  Serial.println("------------------");
+
+  // verbinden met wifi
+  Serial.print("Proberen te connecteren tot het wifi netwerk SSD: ");
+  Serial.println(SECRET_SSID);
+  WiFi.begin( SECRET_SSID, SECRET_PASS ); // verbinden met de wifi
+  while (WiFi.status() != WL_CONNECTED) { 
+    // print . elke seconden zolang de wifi niet verbonden is
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.println();
+  Serial.println("Verbonden met de wifi!");
+  Serial.println();
+
+  // verbinden met mqtt broker
+  Serial.print("Proberen te verbinden met de MQTT broker: ");
+  Serial.println(BROKER);
+    
+  while (!mqttClient.connect(BROKER, PORT)) {
+    Serial.print("MQTT verbinding mislukt! Fout code = ");
+    Serial.println(mqttClient.connectError());
+  }
+
+  Serial.println("Verbonden met de broker!");
+  Serial.println();
+
+  Serial.print("Verbinden met topic: ");
+  Serial.println(TOPIC);
+  Serial.println();
+
+  // subscribe op het topic
+  mqttClient.subscribe(TOPIC);
+
+  // testen van de bmp280 sensor
+  Serial.println( "BMP280 test" );
+
+  // connecteren vab BMP280 sensor met de niet default I2C connection
+  I2Ctwo.begin(SDA_2, SCL_2);  // SDA=32, SCL=33
+  // connecteren van de bmp280 sensor
     if (!bmp.begin( BMP280_ADR )) {
-        Serial.println(ERROR_BMP_NOT_FOUND);
+        Serial.println("Could not find a valid BMP280 sensor, check wiring, address, sensor ID!");
         return;
     }
-    
-    // If the bmp280 sensor is connected successfully, we continue with a few 
-    // more things, as per below code
-    
-    bmp_connected = true; // Since the connection of the bpm280 sensor is OK
-                          // We can now set this global var accordingly
-
-    // Optional: configure oversampling and filter
-    // to obtain more accurate sensor readings
+  
+  bmp_connected = true; // omdat de connection van de bpm280 sensor oké is
+                        // kunnen we de globale var op true zetten
+  
+  // optioneel: stel oversampling en filter in
+  // om nauwkeurigere sensormetingen te verkrijgen
     bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
                   Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
                   Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
                   Adafruit_BMP280::FILTER_X16,      /* Filtering. */
                   Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
 
-    Serial.println("-- Default Test --");
-    Serial.println( "CanSat => BMP-280 successful connected (I2C-2)" );
-
-    Serial.println();
-    Serial.println();
-  Serial.println( "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-  Serial.println( "Sensor Output LED Stearing - Data Transmission starts!");
-    
-  // Connect to WiFi network
-  Serial.println("Establishing WiFi connection...");
-  WiFi.begin( SECRET_SSID, SECRET_PASS );
-  while ( WiFi.status() != WL_CONNECTED) {
-    Serial.print( "." );
-    delay( 1000 );
-  }
-  // Connection to WiFi is success - provide some info about WiFi connection:
-  sprintf( msg, "\nWiFi Connected, IP = %s\n", WiFi.localIP().toString() );
-  Serial.println( msg );
-
-  // Connect to MQTT broker & topic
-  sprintf( msg, "Tracht te verbinden met MQTT broker- op hostadres: %s en topic %s", BROKER, TOPIC );
-  Serial.println( msg );
-
-  while (!mqttClient.connect( BROKER, PORT)) {
-    Serial.print(ERROR_MQTT_CONNECTION_FAILED);
-    Serial.println(mqttClient.connectError());
-    delay( 1000 );
-  }
-
-  //Subscribe on MQTT Topic
-  Serial.print ( "Subscribing on topic: " );
-  Serial.println( TOPIC );
-
-  mqttClient.subscribe( TOPIC );
-
-  sprintf( msg, "Connection with MQTT broker %s on topic %s established!\n", BROKER, TOPIC );
-  Serial.print( msg );
-  Serial.println( "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-  Serial.println("Starting SD Card:");
-  while(!ConnectSD()){
-    Serial.println("Failed to connect with sd card reader!\n");
-  }
-  Serial.printf( "Deleting file %s\n", FILE_NAME );
-  deleteFile( SD, FILE_NAME );
-  delay( 3000 );
-
+  // bmp succesvol verbonden
+  Serial.println();
+  Serial.println( "BMP280 succesvol verbonden!" );
 }
 
-void loop() { 
-    mqttClient.poll();
+void loop() {
+  // behouden van de verbinding met mqttClient
+  mqttClient.poll();
+  // var definieren waar csv in komt
+  String data = ""; 
 
-    bmp_connected = I2C_check(&I2Ctwo, BMP280_ADR);
+  // check of de BMP280 sensor nog steeds verbonden is door I2C communicatie te testen
+  bmp_connected = I2C_check(&I2Ctwo, BMP280_ADR);
 
-    if (bmp_connected) {
-        printValues();
-    } else {
-        Serial.println("ERROR: BMP280 sensor not found!");
-    }
-    
-    
-    delay(DELAY_TIME);
+  if (bmp_connected) {
+      printValues();
+      data = csv_string_data(); // zet de csv in de var data
+      mqtt_pub_csv(TOPIC,data); // publiceert de data (csv) op mqtt broker
+  } else {
+      Serial.println("ERROR: BMP280 sensor not found!");
+  }
+  
+  delay(1000);
 }
 
+// functie die de waardes van bmp280 sensor print
 void printValues() {
-    String csv = "";
-    // Extra safety check
-    if (!bmp_connected) return;
+  
+  // Extra safety check
+  if (!bmp_connected) return;
 
-    float temperature = bmp.readTemperature(); // Read temperature
-    float pressure = bmp.readPressure() / 100.0F; // Read pressure
-    float altitude = bmp.readAltitude(SEALEVELPRESSURE_HPA); // Read altitude
-    csv = String(temperature) + ";" + String(pressure) + ";" + String(altitude); // CSV string
+  // waardes ophalen
+  float temperature = bmp.readTemperature(); 
+  float pressure = bmp.readPressure() / 100.0F; 
+  float altitude = bmp.readAltitude(SEALEVELPRESSURE_HPA);
 
-    Serial.print("Temperature = ");
-    Serial.print( temperature );
-    Serial.println(" °C");
+  // waardes printen
+  Serial.print("Temperature = ");
+  Serial.print( temperature );
+  Serial.println(" °C");
 
-    Serial.print("Pressure = ");
-    Serial.print( pressure );
-    Serial.println(" hPa");
+  Serial.print("Pressure = ");
+  Serial.print( pressure );
+  Serial.println(" hPa");
 
-    Serial.print("Approx. Altitude = ");
-    Serial.print( altitude );
-    Serial.println(" m");
-
-    Serial.println(csv);
-    Serial.println();
-    mqttClient.beginMessage( TOPIC ); // Message beginning on TOPIC
-    mqttClient.print( csv ); // Printing CSV string on mqtt broker
-    mqttClient.endMessage(); // Ending message
-    Serial.println( "Writing to file" );
-    char msg[ 100 ] = "" ;
-    String csv_2 = csv + "\n";
-    csv_2.toCharArray(msg,100);
-    Serial.println( msg );
-    appendFile(SD, FILE_NAME, msg);
-    delay( 1000 );
-
+  Serial.print("Approx. Altitude = ");
+   Serial.print( altitude );
+  Serial.println(" m");
 }
 
+// functie die de waarde in csv string zet
+String csv_string_data(){
+  // Extra safety check
+  if (!bmp_connected) return String(); // returnt een lege string
 
+  // waarde ophalen
+  float temperature = bmp.readTemperature(); // Read temperature
+  float pressure = bmp.readPressure() / 100.0F; // Read pressure
+  float altitude = bmp.readAltitude(SEALEVELPRESSURE_HPA); // Read altitude
+
+  // waarde in csv string zetten
+  String csv = String(temperature) + ";" + String(pressure) + ";" + String(altitude); // CSV string
+
+  return csv;
+}
+
+// sturen van csv string op mqtt broker
+void mqtt_pub_csv(const char *topic, String str_csv){
+  mqttClient.beginMessage( topic ); // bericht sturen op TOPIC
+  mqttClient.print( str_csv ); // sturen van CSV string op mqtt broker
+  mqttClient.endMessage(); // beëindigen van het bericht
+}
+
+// functie die checkt of een I2C apparaat bereikbaar is op het gegeven adres
 bool I2C_check(TwoWire *bus, byte address) {
-    bus->beginTransmission(address);
-    byte error = bus->endTransmission();
-    return (error == 0);
+    bus->beginTransmission(address);    // start communicatie met I2C apparaat
+    byte error = bus->endTransmission(); // beëindig communicatie en krijg foutcode
+    return (error == 0);                // true als geen fout (error=0)
 }
